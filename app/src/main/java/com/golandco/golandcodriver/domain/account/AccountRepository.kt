1c0f38f9f824c1c82289e4cd2caba20ebcf4adca
vitaliy-anatolievych <vitaliy-anatolievych@gmail.com>
2021-10-22 15:49:57 +0300

    Поправлены входящие данные для регистрации аккаунта

--- app/src/main/java/com/golandco/golandcodriver/domain/account/AccountRepository.kt
+++ app/src/main/java/com/golandco/golandcodriver/domain/account/AccountRepository.kt
@@ -1,5 +1,6 @@
 package com.golandco.golandcodriver.domain.account
 
+import com.golandco.golandcodriver.UserRole
 import com.golandco.golandcodriver.domain.exception.Either
 import com.golandco.golandcodriver.domain.exception.Failure
 import com.golandco.golandcodriver.domain.type.None
@@ -12,7 +13,7 @@ import com.golandco.golandcodriver.domain.type.None
 interface AccountRepository {
     fun login(email: String, password: String): Either<Failure, AccountEntity>
     fun logout(): Either<Failure, None>
-    fun register(email: String, name: String, password: String): Either<Failure, None>
+    fun register(call: String, country: String, dateOfBirth: String, email: String, password: String, phoneNumber: String, userRole: UserRole): Either<Failure, None>
     fun forgetPassword(email: String): Either<Failure, None>
 
     fun getCurrentAccount(): Either<Failure, AccountEntity>
