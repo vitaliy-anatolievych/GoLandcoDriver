423c7316c170ed2a46c1d3e22a694444638c0e58
vitaliy-anatolievych <vitaliy-anatolievych@gmail.com>
2021-10-22 15:43:35 +0300

    Добавлен интерфейс, содержащий функции для взаимодействия с аккаунтом.

---
+++ app/src/main/java/com/golandco/golandcodriver/domain/account/AccountRepository.kt
@@ -0,0 +1,24 @@
+package com.golandco.golandcodriver.domain.account
+
+import com.golandco.golandcodriver.domain.exception.Either
+import com.golandco.golandcodriver.domain.exception.Failure
+import com.golandco.golandcodriver.domain.type.None
+
+/**
+ *
+ * Интерфейс, содержащий функции для взаимодействия с аккаунтом.
+ *
+ */
+interface AccountRepository {
+    fun login(email: String, password: String): Either<Failure, AccountEntity>
+    fun logout(): Either<Failure, None>
+    fun register(email: String, name: String, password: String): Either<Failure, None>
+    fun forgetPassword(email: String): Either<Failure, None>
+
+    fun getCurrentAccount(): Either<Failure, AccountEntity>
+
+    fun updateAccountToken(token: String): Either<Failure, None>
+    fun updateAccountLastSeen(): Either<Failure, None>
+
+    fun editAccount(entity: AccountEntity): Either<Failure, AccountEntity>
+}
