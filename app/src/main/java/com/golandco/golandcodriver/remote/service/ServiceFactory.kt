dcfde292804811ad4f971587cf532acf4974a9fa
vitaliy-anatolievych <vitaliy-anatolievych@gmail.com>
2021-10-22 17:58:57 +0300

    Добавлен ServiceFactory для создания API сервиса.

---
+++ app/src/main/java/com/golandco/golandcodriver/remote/service/ServiceFactory.kt
@@ -0,0 +1,49 @@
+package com.golandco.golandcodriver.remote.service
+
+import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+/**
+ * Для создания API сервиса.
+ */
+object ServiceFactory {
+    const val BASE_URL = "http://a623-94-154-232-220.ngrok.io"
+
+    fun makeService(isDebug: Boolean): ApiService {
+        val okHttpClient = makeOkHttpClient(
+            makeLoggingInterceptor((isDebug))
+        )
+        return makeService(okHttpClient, Gson())
+    }
+
+    private fun makeService(okHttpClient: OkHttpClient, gson: Gson): ApiService {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+        return retrofit.create(ApiService::class.java)
+    }
+
+    private fun makeOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .build()
+    }
+
+    private fun makeLoggingInterceptor(isDebug: Boolean): HttpLoggingInterceptor {
+        val logging = HttpLoggingInterceptor()
+        logging.level = if (isDebug) {
+            HttpLoggingInterceptor.Level.BODY
+        } else {
+            HttpLoggingInterceptor.Level.NONE
+        }
+        return logging
+    }
+}
