2ac463c6e9ba46ba24069c8b4e35410074ff47eb
vitaliy-anatolievych <vitaliy-anatolievych@gmail.com>
2021-10-22 17:20:20 +0300

    Добавлен AccountCacheImpl.kt для взаимодействия с аккаунтом в бд.

---
+++ app/src/main/java/com/golandco/golandcodriver/cache/AccountCacheImpl.kt
@@ -0,0 +1,18 @@
+package com.golandco.golandcodriver.cache
+
+import com.golandco.golandcodriver.data.account.AccountCache
+import com.golandco.golandcodriver.domain.exception.Either
+import com.golandco.golandcodriver.domain.exception.Failure
+import com.golandco.golandcodriver.domain.type.None
+import javax.inject.Inject
+
+class AccountCacheImpl @Inject constructor(private val prefsManager: SharedPrefsManager): AccountCache {
+
+    override fun saveToken(token: String): Either<Failure, None> {
+        return prefsManager.saveToken(token)
+    }
+
+    override fun getToken(): Either<Failure, String> {
+        return prefsManager.getToken()
+    }
+}
